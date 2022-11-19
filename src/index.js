50e6418d6118b05c7a173892fd761bfc3acc8cdf
khanhnd <khanh.nttu0204@gmail.com>
2022-11-19 10:31:59 +0700

    create-model-connect-db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 dotenv.config()
 
 const app = express()
@@ -8,6 +9,14 @@ const port = process.env.PORT || 3001
 app.get('/', (req, res) => {
     res.send('Hello World! everryone')
 })
+
+mongoose.connect(`mongodb+srv://lttd:${process.env.MONGO_DB}@cluster0.xifiufo.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Connect Db success!')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.listen(port, () => {
     console.log('Server is running in port: ', + port)

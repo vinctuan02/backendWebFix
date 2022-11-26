dfdbc6cfafb46a06e2d4e5614d4e4266ab68cb45
khanhnd <khanh.nttu0204@gmail.com>
2022-11-26 19:47:22 +0700

    update-api-panigated-backend

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -102,14 +102,18 @@ const getDetailsProduct = (id) => {
     })
 }
 
-const getAllProduct = () => {
+const getAllProduct = (limit = 8, page = 0) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const allProduct = await Product.find()
+            const totalProduct = await Product.count()
+            const allProduct = await Product.find().limit(limit).skip(page * limit)
             resolve({
                 status: 'OK',
                 message: 'Success',
-                data: allProduct
+                data: allProduct,
+                total: totalProduct,
+                pageCurrent: Number(page + 1),
+                totalPage: Math.ceil(totalProduct / limit)
             })
         } catch (e) {
             reject(e)

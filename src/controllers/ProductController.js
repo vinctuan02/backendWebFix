6a8d24a64532fa0e9e35650f1c6d0e683e1b4fac
khanhnd <khanh.nttu0204@gmail.com>
2022-11-26 11:40:59 +0700

    update-api-product

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,94 @@
+const ProductService = require('../services/ProductService')
+
+const createProduct = async (req, res) => {
+    try {
+        const { name, image, type, countInStock, price, rating, description } = req.body
+
+        if (!name || !image || !type || !countInStock || !price || !rating) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }
+        const response = await ProductService.createProduct(req.body)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const updateProduct = async (req, res) => {
+    try {
+        const productId = req.params.id
+        const data = req.body
+        if (!productId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The productId is required'
+            })
+        }
+        const response = await ProductService.updateProduct(productId, data)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const getDetailsProduct = async (req, res) => {
+    try {
+        const productId = req.params.id
+        if (!productId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The productId is required'
+            })
+        }
+        const response = await ProductService.getDetailsProduct(productId)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const productId = req.params.id
+        if (!productId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The productId is required'
+            })
+        }
+        const response = await ProductService.deleteProduct(productId)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const getAllProduct = async (req, res) => {
+    try {
+        const response = await ProductService.getAllProduct()
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+module.exports = {
+    createProduct,
+    updateProduct,
+    getDetailsProduct,
+    deleteProduct,
+    getAllProduct
+}
---
+++ src/services/ProductService.js
@@ -0,0 +1,128 @@
+const Product = require("../models/ProductModel")
+
+const createProduct = (newProduct) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, image, type, countInStock, price, rating, description } = newProduct
+        try {
+            const checkProduct = await Product.findOne({
+                name: name
+            })
+            if (checkProduct !== null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The name of product is already'
+                })
+            }
+            const newProduct = await Product.create({
+                name, image, type, countInStock, price, rating, description
+            })
+            if (newProduct) {
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: newProduct
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const updateProduct = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkProduct = await Product.findOne({
+                _id: id
+            })
+            if (checkProduct === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The product is not defined'
+                })
+            }
+
+            const updatedProduct = await Product.findByIdAndUpdate(id, data, { new: true })
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedProduct
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteProduct = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkProduct = await Product.findOne({
+                _id: id
+            })
+            if (checkProduct === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The product is not defined'
+                })
+            }
+
+            await Product.findByIdAndDelete(id)
+            resolve({
+                status: 'OK',
+                message: 'Delete product success',
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getDetailsProduct = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const product = await Product.findOne({
+                _id: id
+            })
+            if (product === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The product is not defined'
+                })
+            }
+
+            resolve({
+                status: 'OK',
+                message: 'SUCESS',
+                data: product
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getAllProduct = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allProduct = await Product.find()
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allProduct
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+
+module.exports = {
+    createProduct,
+    updateProduct,
+    getDetailsProduct,
+    deleteProduct,
+    getAllProduct
+}

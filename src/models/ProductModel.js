52699ea74b14f6d6fbe68d3dd0a6bea25c4619c2
khanhnd <khanh.nttu0204@gmail.com>
2022-12-10 17:13:25 +0700

    update-model-backend

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -9,6 +9,8 @@ const productSchema = new mongoose.Schema(
         countInStock: { type: Number, required: true },
         rating: { type: Number, required: true },
         description: { type: String },
+        discount: { type: Number },
+        selled: { type: Number },
     },
     {
         timestamps: true,

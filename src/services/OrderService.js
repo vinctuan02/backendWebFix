4adffbc2c4aebffb6989841f79a75475ee84407b
khanhnd <khanh.nttu0204@gmail.com>
2023-03-22 22:32:58 +0700

    apply-nodemailer

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,9 +1,10 @@
 const Order = require("../models/OrderProduct")
 const Product = require("../models/ProductModel")
+const EmailService = require("../services/EmailService")
 
 const createOrder = (newOrder) => {
     return new Promise(async (resolve, reject) => {
-        const { orderItems,paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone,user, isPaid, paidAt } = newOrder
+        const { orderItems,paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone,user, isPaid, paidAt,email } = newOrder
         try {
             const promises = orderItems.map(async (order) => {
                 const productData = await Product.findOneAndUpdate(
@@ -33,6 +34,7 @@ const createOrder = (newOrder) => {
                         isPaid, paidAt
                     })
                     if (createdOrder) {
+                        await EmailService.sendEmailCreateOrder(email,orderItems)
                         return {
                             status: 'OK',
                             message: 'SUCCESS'

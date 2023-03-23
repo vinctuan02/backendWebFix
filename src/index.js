4a7cb5c4ac18a25f81296e5db502dbf44d449cd0
khanhnd <khanh.nttu0204@gmail.com>
2023-03-23 16:00:47 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://shop-lttd.onrender.com/']
-}))
+app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json())

1843bc9e25d8324fd0ce9468af517609a4575a37
khanhnd <khanh.nttu0204@gmail.com>
2023-03-23 17:03:05 +0700

    update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -51,9 +51,9 @@ const loginUser = async (req, res) => {
         const { refresh_token, ...newReponse } = response
         res.cookie('refresh_token', refresh_token, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            path: '/,
+            // secure: true,
+            // sameSite: 'strict',
+            // path: '/,
         })
         return res.status(200).json(newReponse)
     } catch (e) {

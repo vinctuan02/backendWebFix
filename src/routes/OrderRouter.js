914b76d9c6c75078f1a2a072fa281ac6f08ce182
khanhnd <khanh.nttu0204@gmail.com>
2023-04-08 20:46:25 +0700

    update-create-orrder

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const OrderController = require('../controllers/OrderController');
 const { authUserMiddleWare, authMiddleWare } = require("../middleware/authMiddleware");
 
-router.post('/create', authUserMiddleWare, OrderController.createOrder)
+router.post('/create', OrderController.createOrder)
 router.get('/get-all-order/:id',authUserMiddleWare, OrderController.getAllOrderDetails)
 router.get('/get-details-order/:id',authUserMiddleWare, OrderController.getDetailsOrder)
 router.delete('/cancel-order/:id',authUserMiddleWare, OrderController.cancelOrderDetails)

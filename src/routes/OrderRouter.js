9c1a95414a9e23ab4763557e9cf6fae2a497f2cc
khanhnd <khanh.nttu0204@gmail.com>
2023-04-10 22:05:37 +0700

    git add details order

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -5,7 +5,7 @@ const { authUserMiddleWare, authMiddleWare } = require("../middleware/authMiddle
 
 router.post('/create/:id', authUserMiddleWare, OrderController.createOrder)
 router.get('/get-all-order/:id',authUserMiddleWare, OrderController.getAllOrderDetails)
-router.get('/get-details-order/:id',authUserMiddleWare, OrderController.getDetailsOrder)
+router.get('/get-details-order/:id', OrderController.getDetailsOrder)
 router.delete('/cancel-order/:id',authUserMiddleWare, OrderController.cancelOrderDetails)
 router.get('/get-all-order',authMiddleWare, OrderController.getAllOrder)
 
